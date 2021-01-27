7e73265e4d7ce64e8ae39375ce800d829b83b77b
bnlrnz <lorenzb@tu-freiberg.de>
2021-01-27 10:31:00 +0100

    jonas approved formatting

--- 12 - Qos, Hashes/MD5Collision/strong_collision_resistance.cpp
+++ 12 - Qos, Hashes/MD5Collision/strong_collision_resistance.cpp
@@ -9,63 +9,69 @@
 
 #define STRING_LENGTH 256
 
-void randomString(unsigned char *dest, unsigned int length){
+void randomString(unsigned char *dest, unsigned int length)
+{
 	char charset[] = "0123456789"
-    				"abcdefghijklmnopqrstuvwxyz"
-                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+					 "abcdefghijklmnopqrstuvwxyz"
+					 "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	while (length-- > 1) {
-        size_t index = (double) rand() / RAND_MAX * (sizeof charset - 1);
-        *dest++ = charset[index];
-    }
-    *dest = '\0';             	
+	while (length-- > 1)
+	{
+		size_t index = (double)rand() / RAND_MAX * (sizeof charset - 1);
+		*dest++ = charset[index];
+	}
+	*dest = '\0';
 }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 
 	int collisionLength = 5;
 
 	std::map<std::string, std::string> map;
 
 	//get command line argument
-	if (argc >= 2){
+	if (argc >= 2)
+	{
 		collisionLength = atoi(argv[1]);
 	}
 
 	//init seed with current time
 	srand(time(NULL));
-	
+
 	bool didCollide = false;
 	unsigned int tries = 0;
 
 	clock_t start = clock();
-	while(!didCollide){
+	while (!didCollide)
+	{
 		unsigned char message[STRING_LENGTH];
 		memset(message, 0, STRING_LENGTH);
 		randomString(message, STRING_LENGTH);
 
-    	char fullHash[33] = {0};
-		md5(message, strlen((char*)message), (unsigned char*)fullHash);
+		char fullHash[33] = {0};
+		md5(message, strlen((char *)message), (unsigned char *)fullHash);
 
-    	char* shortHash = (char*)malloc(collisionLength+1);
-    	memset(shortHash, 0, collisionLength+1);
-    	memcpy(shortHash, fullHash, collisionLength);
+		char *shortHash = (char *)malloc(collisionLength + 1);
+		memset(shortHash, 0, collisionLength + 1);
+		memcpy(shortHash, fullHash, collisionLength);
 
-    	if(!map.insert(std::make_pair(shortHash, fullHash)).second){
-    	 	didCollide = true;
-    	 	printf("Target Hash: %s\n", fullHash);
-    	 	printf("Found SHash: %s\n", shortHash);
-    	 	printf("Found FHash: %s\n", map[shortHash].c_str());
-    	}
+		if (!map.insert(std::make_pair(shortHash, fullHash)).second)
+		{
+			didCollide = true;
+			printf("Target Hash: %s\n", fullHash);
+			printf("Found SHash: %s\n", shortHash);
+			printf("Found FHash: %s\n", map[shortHash].c_str());
+		}
 
 		free(shortHash);
-    	tries++;
+		tries++;
 	}
 
 	clock_t end = clock();
 
 	printf("Found Collision with length %d in %d tries\n", collisionLength, tries);
-	printf("Time: %.5f sec\n", (double)(end - start)/CLOCKS_PER_SEC);
+	printf("Time: %.5f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
 
 	return 0;
 }
--- 12 - Qos, Hashes/MD5Collision/weak_collision_resistance.c
+++ 12 - Qos, Hashes/MD5Collision/weak_collision_resistance.c
@@ -8,30 +8,34 @@
 
 #define STRING_LENGTH 256
 
-void randomString(unsigned char *dest, unsigned int length){
-	    char charset[] = "0123456789"
-                     	 "abcdefghijklmnopqrstuvwxyz"
-                     	 "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+void randomString(unsigned char *dest, unsigned int length)
+{
+	char charset[] = "0123456789"
+					 "abcdefghijklmnopqrstuvwxyz"
+					 "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	while (length-- > 1) {
-        size_t index = (double) rand() / RAND_MAX * (sizeof charset - 1);
-        *dest++ = charset[index];
-    }
-    *dest = '\0';             	
+	while (length-- > 1)
+	{
+		size_t index = (double)rand() / RAND_MAX * (sizeof charset - 1);
+		*dest++ = charset[index];
+	}
+	*dest = '\0';
 }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 
 	int collisionLength = 4;
 
 	//get command line argument
-	if (argc >= 2){
+	if (argc >= 2)
+	{
 		collisionLength = atoi(argv[1]);
 	}
 
 	//init seed with current time
 	srand(time(NULL));
-	
+
 	unsigned char message[STRING_LENGTH];
 	memset(message, 0, STRING_LENGTH);
 	randomString(message, STRING_LENGTH);
@@ -40,28 +44,29 @@ int main(int argc, char** argv){
 	memset(mdTargetString, 0, 33);
 	md5(message, STRING_LENGTH, mdTargetString);
 
-    //roll the dice
+	//roll the dice
 	unsigned char anotherMessage[STRING_LENGTH];
 	memset(anotherMessage, 0, STRING_LENGTH);
-	
+
 	unsigned char mdString[33];
 	memset(mdString, 0, 33);
 
 	unsigned int tries = 0;
-        
+
 	clock_t start = clock();
-    while(strncmp((char*)mdString, (char*)mdTargetString, collisionLength)){
-    	randomString(anotherMessage, STRING_LENGTH);
-   		md5(anotherMessage, STRING_LENGTH, mdString);
-        tries++;
-    }
+	while (strncmp((char *)mdString, (char *)mdTargetString, collisionLength))
+	{
+		randomString(anotherMessage, STRING_LENGTH);
+		md5(anotherMessage, STRING_LENGTH, mdString);
+		tries++;
+	}
 	clock_t end = clock();
 
 	printf("Target Hash: %s\n", mdTargetString);
 	printf("Found  Hash: %s\n", mdString);
-	
+
 	printf("Found Collision with length %d in %d tries\n", collisionLength, tries);
-	printf("Time: %.5f sec\n", (double)(end - start)/CLOCKS_PER_SEC);
+	printf("Time: %.5f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
 
 	return 0;
 }
